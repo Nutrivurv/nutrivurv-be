5315f04ed90b149fd188d9a82a41ad8eb7aae62f
benjamin-t-wilson <benjiimanwillson@gmail.com>
2020-02-11 20:51:58 -0700

    added ability to grab profile and dailyrecords off of user object

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,18 +1,40 @@
-import getUserId from '../utils/getUserId'
+import getUserId from "../utils/getUserId";
 
 const User = {
-    email: {
-        fragment: 'fragment userId on User { id }',
-        resolve(parent, args, { request }, info) {
-            const userId = getUserId(request, false)
+  email: {
+    fragment: "fragment userId on User { id }",
+    resolve(parent, args, { request }, info) {
+      const userId = getUserId(request, false);
 
-            if (userId && userId === parent.id) {
-                return parent.email
-            } else {
-                return null
-            }
-        }
+      if (userId && userId === parent.id) {
+        return parent.email;
+      } else {
+        return null;
+      }
     }
-}
+  },
+  profile: {
+    fragment: "fragment userId on User { id }",
+    resolve(parent, args, { prisma, request }, info) {
+      const userId = getUserId(request, false);
+      return prisma.query.profile({
+        where: {
+          user_id: userId
+        }
+      });
+    }
+  },
+  dailyRecords: {
+    fragment: "fragment userId on User { id }",
+    resolve(parent, args, { prisma, request }, info) {
+      const userId = getUserId(request, false);
+      return prisma.query.dailyRecords({
+        where: {
+          user_id: userId
+        }
+      });
+    }
+  }
+};
 
-export { User as default }
+export { User as default };
