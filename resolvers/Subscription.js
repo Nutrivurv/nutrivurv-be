8cfcb5400cee9ece7cabf2dc0e751de892cf1d56
benjamin-t-wilson <benjiimanwillson@gmail.com>
2020-04-15 20:06:40 -0700

    added previous values

--- resolvers/Subscription.js
+++ resolvers/Subscription.js
@@ -1,29 +1,3 @@
-// const Subscription = {
-//   comment: {
-//     subscribe(parent, { postId }, { prisma }, info) {
-//       return prisma.subscription.comment(
-//         {
-//           where: {
-//             node: {
-//               post: {
-//                 id: postId,
-//               },
-//             },
-//           },
-//         },
-//         info
-//       );
-//     },
-//   },
-//   post: {
-//     subscribe(parent, args, { prisma }, info) {
-//       return prisma.subscription.post(info);
-//     },
-//   },
-// };
-
-// export { Subscription as default };
-
 const Subscription = {
   comment: {
     subscribe(parent, { postId }, { prisma }, info) {
