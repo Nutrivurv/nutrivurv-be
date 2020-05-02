7d4cd7dfc6ffa494bd25a71ee430d8bada3eb0df
benjamin-t-wilson <benjiimanwillson@gmail.com>
2020-05-02 08:57:02 -0700

    finished todays punchlist

--- resolvers/queries/weightLogs.js
+++ resolvers/queries/weightLogs.js
@@ -14,6 +14,18 @@ const WeightLogs = {
       return null;
     }
   },
+  async myWeightLogCount(parent, args, { prisma, request }, info) {
+    const count = await prisma.query.weightLogs({
+      where: {
+        user_id: request.user_id,
+      },
+    });
+    if (count && count.length > 0) {
+      return count;
+    } else {
+      return 0;
+    }
+  },
   weightLog(parent, args, { prisma, request }, info) {
     if (request.user_id) {
       return prisma.query.weightLog({
--- utils/getUserId.js
+++ utils/getUserId.js
@@ -1,25 +1,32 @@
 const jwt = require("jsonwebtoken");
 
 const isLoggedIn = async (resolve, parent, args, ctx, info) => {
-  if (
-    ctx.request.request.headers.cookie &&
-    ctx.request.request.headers.cookie.includes("token=")
-  ) {
-    const cookiePermit = ctx.request.request.headers.cookie
-      .split("; ")
-      .filter((cv) => {
-        return cv.includes("token");
-      })[0]
-      .split("=")[1];
-    const cookieToken = cookiePermit.replace("Bearer ", "");
-    const decodedCookie = jwt.verify(cookieToken, process.env.JWT_SECRET);
-    ctx.request.user_id = decodedCookie.userId;
-    return resolve();
-  }
+  const permit = () => {
+    if (ctx.request.request) {
+      if (ctx.request.request.headers) {
+        if (ctx.request.request.headers.authorization) {
+          return ctx.request.request.headers.authorization;
+        } else if (
+          ctx.request.request.headers.cookie &&
+          ctx.request.request.headers.cookie.includes("token=")
+        ) {
+          return ctx.request.request.headers.cookie
+            .split("; ")
+            .filter((cv) => {
+              return cv.includes("token");
+            })[0]
+            .split("=")[1];
+        }
+      }
+    } else if (ctx.request.connection) {
+      if (ctx.request.connection.context) {
+        if (ctx.request.connection.context.Authorization) {
+          return ctx.request.connection.context.Authorization;
+        }
+      }
+    }
+  };
 
-  const permit = ctx.request.request
-    ? ctx.request.request.headers.authorization
-    : ctx.request.connection.context.Authorization;
   if (ctx.request.request) {
     if (
       ctx.request.request.body.query.includes("login") ||
@@ -28,11 +35,16 @@ const isLoggedIn = async (resolve, parent, args, ctx, info) => {
       return resolve();
     }
   }
-  if (!permit) {
+
+  if (!permit()) {
     throw new Error(`Not authorised!`);
   }
-  const token = permit.replace("Bearer ", "");
+
+  const token = permit().replace("Bearer ", "");
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  if (!decoded || !decoded.userId) {
+    throw new Error(`Not authorised!`);
+  }
   ctx.request.user_id = decoded.userId;
   return resolve();
 };
