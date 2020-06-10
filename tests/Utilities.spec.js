c7def0ca2cfeafb0d02ab952d202da25e1602f11
Cody Brewer <codybrewerdev@gmail.com>
2020-06-09 20:06:57 -0600

    formateDate() tests

--- tests/Utilities.spec.js
+++ tests/Utilities.spec.js
@@ -1,6 +1,7 @@
 const hashPassword = require('../utils/hashPassword.js');
 const validateLogin = require('../utils/validateLogin.js');
 const authorization = require('../utils/authorization');
+const formatDate = require('../utils/formatDate');
 const jwt = require('jsonwebtoken');
 
 describe('utility test cases', () => {
@@ -106,5 +107,28 @@ describe('utility test cases', () => {
     );
 
     expect(loggedIn).toBe('yay');
+  });
+  describe('formatDate()', () => {
+    const expected = '09-06-2020';
+    test('formats "MM/DD/YYYY" date string into "DD-MM-YYYY', () => {
+      const date = '06/09/2020'
+            
+      expect(formatDate(date)).toBe(expected);
+    });
+    test('formats "MM-DD-YY" date string into "DD-MM-YYYY"', () => {
+      const date = '06/09/2020';
+      
+      expect(formatDate(date)).toBe(expected);
+    });
+    test('formats "AbrMon DD, YYYY" date string into "DD-MM-YYYY"', () => {
+      const date = 'Jun 09, 2020';
+      
+      expect(formatDate(date)).toBe(expected);
+    });
+    test('formats "Month DD, YYYY" date string in "DD-MM-YYYY"', () => {
+      const date = 'June 09, 2020';
+      
+      expect(formatDate(date)).toBe(expected);
+    });
   });
 });
