46366992bb53ae925047c7d804556fd967a1ad39
Cody Brewer <codybrewerdev@gmail.com>
2020-06-11 21:13:07 -0600

    🔒 add third-party middleware
    set up express.json, helemt, and cors middleware"

--- api/server.js
+++ api/server.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 
 const server = express();
+
+//Third Party Middleware
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
 
 module.exports = server;
