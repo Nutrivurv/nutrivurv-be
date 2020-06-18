4dd0795716c60ba267ae6837b9c2ba767602b491
Cody Brewer <codybrewerdev@gmail.com>
2020-06-17 20:13:29 -0600

    🗃 Seed Users
    seed for user data

---
+++ db/seeds/development/01-users.js
@@ -0,0 +1,34 @@
+exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  return knex('users')
+    .del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        {
+          userID: 1,
+          username: 'Lou',
+          email: 'lou@news.com',
+          password: 'Abadpassword',
+          created_at: '2020-06-17 07:56:00',
+          updated_at: '2020-06-17 07:56:00',
+        },
+        {
+          userID: 2,
+          username: 'stu',
+          email: 'stu@news.com',
+          password: 'AB3TT3rassword',
+          created_at: '2020-06-17 07:57:00',
+          updated_at: '2020-06-17 07:57:00',
+        },
+        {
+          userID: 3,
+          username: 'Hugh',
+          email: 'hugh@news.com',
+          password: 'anotherpass',
+          created_at: '2020-06-17 07:59:00',
+          updated_at: '2020-06-17 07:59:00',
+        },
+      ]);
+    });
+};
