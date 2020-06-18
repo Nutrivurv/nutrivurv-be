0aabdb19783b7b3d381bc570ed12f40be1ab4cdb
Cody Brewer <codybrewerdev@gmail.com>
2020-06-17 20:26:46 -0600

    👌 change test to test against api/status for the server status

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -2,16 +2,16 @@ const request = require('supertest');
 const server = require('../api/server.js');
 
 describe('server', () => {
-  test('GET /api should return status 200 OK', async () => {
-    const response = await request(server).get('/api');
+  test('GET /api/status should return status 200 OK', async () => {
+    const response = await request(server).get('/api/status');
     expect(response.status).toBe(200);
   });
   test('GET /api should return JSON data type', async () => {
-    const response = await request(server).get('/api');
+    const response = await request(server).get('/api/status');
     expect(response.type).toMatch(/json/i)
   });
   test('GET /api should return json with body of { status: "up" }', async () => {
-    const response = await request(server).get('/api');
+    const response = await request(server).get('/api/status');
     expect(response.body.status).toBe('up');
   });
 });
