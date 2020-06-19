2f8be7736796f96f83834d3193f5f456255bb787
Cody Brewer <codybrewerdev@gmail.com>
2020-06-18 21:23:14 -0600

    🗃 Add user profile columns
    added columns needed for profile data

--- db/migrations/20200616193923_create_users_table.js
+++ db/migrations/20200616193923_create_users_table.js
@@ -3,11 +3,30 @@ exports.up = function (knex) {
     // id
     table.increments();
     // name
-    table.string('username', 64);
+    table.string('username').notNullable();
     // email
-    table.string('email', 254);
+    table.string('email').notNullable();
     // password
-    table.string('password', 16);
+    table.string('password').notNullable();
+    // date of birth
+    table.date('date_of_birth').notNullable();
+    // gender
+    table.enu('gender', ['male', 'female', 'non-binary']).notNullable();
+    // goal weight
+    table.integer('goal_weight').notNullable();
+    // activity level
+    table.enu('activity_level', [1.375, 1.55, 1.725, 1.9]).notNullable();
+    // diertary preference
+    table.enu('dietary_preference', [0, 1, 2, 3]).notNullable();
+    // height in imperial units
+    table.integer('feet');
+    table.integer('inches');
+    // weight in imperial units
+    table.integer('pounds');
+    // height in metric
+    table.integer('centimeters');
+    // weight in metric
+    table.integer('kilograms');
     // create_at & update_at timestamps
     table.timestamps();
   });
