c68c7a1e5b39843a7978d77b89388f272c3e004f
Cody Brewer <codybrewerdev@gmail.com>
2020-06-19 22:01:35 -0600

    🐛 Fix can only call endpoint once
    setting db('users') to query and then calling query caused this weird bug for some reason.

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,10 +1,9 @@
 const db = require('../../db/config');
-
-const query = db('users');
+const table = 'users';
 
 const getAllUsers = async () => {
   try {
-    const users = await query;
+    const users = await db(table);
     return users
   } catch (error) {
     return error    
@@ -13,8 +12,7 @@ const getAllUsers = async () => {
 
 const getUserBy = async (filter) => {
   try {
-    const user = await query.where(filter).first();
-    
+    const user = await db(table).where(filter).first();
     return user
   } catch (error) {
     return error 
@@ -30,7 +28,7 @@ const addUser = async ({ username, email, password}) => {
     updated_at: Date.now()
   }
   try {
-    const inserted = await query.insert(user, {userID, username, email});
+    const inserted = await db(table).insert(user, {userID, username, email});
     return inserted;
   } catch (error) {
     return error
@@ -40,7 +38,7 @@ const addUser = async ({ username, email, password}) => {
 const updateUser = async (userID, changes) => {
   const userChanges = {...changes, updated_at: Date.now()}
   try {
-    const updated = await query.where(userID).update(userChanges)
+    const updated = await db(table).where(userID).update(userChanges)
     return updated;
   } catch (error) {
     return error
@@ -49,7 +47,7 @@ const updateUser = async (userID, changes) => {
 
 const  deleteUser = async (userID) => {
   try {
-    const deleted = await query.where({ userID }).del()
+    const deleted = await db(table).where({ userID }).del()
     return deleted;
   } catch (error) {
     return error
