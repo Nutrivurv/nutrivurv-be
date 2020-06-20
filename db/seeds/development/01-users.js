a01822d376bf0ae28b8ee908a83c0a9ec5496ea1
Cody Brewer <codybrewerdev@gmail.com>
2020-06-19 22:06:07 -0600

    🎨 hash seeded password
    slimmed seed down to one user, add hashing password.

--- db/seeds/development/01-users.js
+++ db/seeds/development/01-users.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const hash = bcrypt.hashSync(process.env.USER, 10);
+
 exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex('users')
@@ -6,10 +9,10 @@ exports.seed = function (knex) {
       // Inserts seed entries
       return knex('users').insert([
         {
-          id: 1,
+          id: 0,
           username: 'Lou',
           email: 'lou@news.com',
-          password: 'Abadpassword',
+          password: hash,
           date_of_birth: '09-09-1985',
           gender: 'male',
           goal_weight: 230,
@@ -22,41 +25,6 @@ exports.seed = function (knex) {
           kilograms: 260,
           created_at: '2020-06-17 07:56:00',
           updated_at: '2020-06-17 07:56:00',
-        },
-        {
-          id: 2,
-          username: 'stu',
-          email: 'stu@news.com',
-          password: 'AB3TT3rassword',
-          date_of_birth: '10-10-1995',
-          gender: 'male',
-          goal_weight: 200,
-          activity_level: 1.9,
-          dietary_preference: 0,
-          feet: 5,
-          inches: 11,
-          pounds: 240,
-          centimeters: null,
-          kilograms: null,
-          created_at: '2020-06-17 07:57:00',
-          updated_at: '2020-06-17 07:57:00',
-        },
-        {
-          id: 3,
-          username: 'Hugh',
-          email: 'hugh@news.com',
-          password: 'anotherpass',
-          date_of_birth: '11-11-1973',
-          gender: 'male',
-          goal_weight: 180,
-          activity_level: 1.55,
-          dietary_preference: 1,
-          feet: null,
-          inches: null,
-          centimeters: 160,
-          kilograms: 200,
-          created_at: '2020-06-17 07:59:00',
-          updated_at: '2020-06-17 07:59:00',
         },
       ]);
     });
