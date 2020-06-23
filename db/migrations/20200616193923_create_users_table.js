39639fb2496c186f1d45bfd52a3882a98a3da225
Cody Brewer <codybrewerdev@gmail.com>
2020-06-22 20:00:37 -0600

    🔥 remove profile columns & rename username column

--- db/migrations/20200616193923_create_users_table.js
+++ db/migrations/20200616193923_create_users_table.js
@@ -3,32 +3,11 @@ exports.up = function (knex) {
     // id
     table.increments();
     // name
-    table.string('username').notNullable().unique();
+    table.string('name');
     // email
-    table.string('email').notNullable().unique();
+    table.string('email').unique();
     // password
-    table.string('password').notNullable();
-    // date of birth
-    table.date('date_of_birth').notNullable();
-    // gender
-    table.enu('gender', ['male', 'female', 'non-binary']).notNullable();
-    // goal weight
-    table.integer('goal_weight').notNullable();
-    // activity level
-    table.enu('activity_level', [1.375, 1.55, 1.725, 1.9]).notNullable();
-    // diertary preference
-    table.enu('dietary_preference', [0, 1, 2, 3]).notNullable();
-    // height in imperial units
-    table.integer('feet');
-    table.integer('inches');
-    // weight in imperial units
-    table.integer('pounds');
-    // height in metric
-    table.integer('centimeters');
-    // weight in metric
-    table.integer('kilograms');
-    // create_at & update_at timestamps
-    table.timestamps();
+    table.string('password');
   });
 };
 
--- db/seeds/development/01-users.js
+++ db/seeds/development/01-users.js
@@ -10,21 +10,9 @@ exports.seed = function (knex) {
       return knex('users').insert([
         {
           id: 0,
-          username: 'Lou',
+          name: 'Lou',
           email: 'lou@news.com',
           password: hash,
-          date_of_birth: '09-09-1985',
-          gender: 'male',
-          goal_weight: 230,
-          activity_level: 1.375,
-          dietary_preference: 2,
-          feet: null,
-          inches: null,
-          pounds: null,
-          centimeters: 200,
-          kilograms: 260,
-          created_at: '2020-06-17 07:56:00',
-          updated_at: '2020-06-17 07:56:00',
         },
       ]);
     });
