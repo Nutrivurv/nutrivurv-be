b294827a3a3fc49d1dbc7aa1d4095163a30fe5f5
Cody Brewer <codybrewerdev@gmail.com>
2020-06-22 20:17:50 -0600

    🔥 remove users route
    need for a users/ route does not exist currently

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,12 +1,10 @@
 const router = require('express').Router();
-const usersRouter = require('./users');
 const authRouter = require('./auth');
 
 router.get('/status', (req, res) => {
   res.send({ status: 'up' });
 });
 
-router.use('/users', usersRouter);
 router.use('/auth', authRouter);
 
 module.exports = router;
