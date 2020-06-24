4b3539c893a0278b25be50a7c3b0277a2e0886b0
Dave Vazquez <dmvaz.li@gmail.com>
2020-06-23 23:32:10 -0400

    adds middleware to register/login end-points

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,40 +2,52 @@ const router = require('express').Router();
 const { Users } = require('../controllers');
 const hashPassword = require('../../utils/hashPassword');
 const generateToken = require('../../utils/generateToken');
+const { getUserBy } = require('../controllers/users');
+const bcrypt = require('bcryptjs');
+const validateRegistration = require('../../utils/validateRegistration');
 const validateLogin = require('../../utils/validateLogin');
 
-router.post('/register', async (req, res) => {
-  const registrationDetails = req.body;
-  const hash = await hashPassword(registrationDetails.password);
-  const newUser = { ...registrationDetails, password: hash };
+router.post('/register', validateRegistration, async (req, res) => {
+  let newUser = req.body;
+  newUser.password = await hashPassword(req.body.password);
 
   try {
-    const registeredUser = await Users.addUser(newUser);
-    const token = generateToken(registeredUser.id);
+    newUser = await Users.addUser(newUser);
 
     res.status(201).json({
-      message: `${registeredUser.email} registered!`,
-      token,
+      message: `${newUser.email} has been registered.`,
+      token: generateToken(newUser.id),
     });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({
+      message: 'Internal Server Error',
+      error: error.message,
+    });
   }
 });
 
-router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
+router.post('/login', validateLogin, async (req, res) => {
+  let user = req.body;
+  let password = user.password;
 
   try {
-    const user = await Users.getUserBy({ email });
+    user = await Users.getUserBy({ email: user.email });
 
-    const checkUser = { email: user.email, password: user.password };
-
-    if (checkUser) {
-      const { user, token } = await validateLogin(password, checkUser);
-      res.status(200).json({ message: `Welcome ${user.email}`, token });
+    if (!(await bcrypt.compare(password, user.password))) {
+      res.status(400).json({
+        message: 'Invalid credentials',
+      });
     }
+
+    res.status(200).json({
+      message: `${user.email} has been authenticated.`,
+      token: generateToken(user.id),
+    });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({
+      message: 'Internal Server Error',
+      error: error.message,
+    });
   }
 });
 
