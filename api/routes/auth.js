8a774342f8406d05efb98bcb7cd7325999dba7bd
Dave Vazquez <dmvaz.li@gmail.com>
2020-07-12 23:38:01 -0400

    updates auth router to return back budgets

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,25 +1,39 @@
 const router = require('express').Router();
 const { Users } = require('../controllers');
-const hashPassword = require('../../utils/hashPassword');
-const generateToken = require('../../utils/generateToken');
-const { getUserBy } = require('../controllers/users');
+const generateToken = require('../middleware/generateToken');
 const bcrypt = require('bcryptjs');
-const validateRegistration = require('../../utils/validateRegistration');
-const validateLogin = require('../../utils/validateLogin');
-
+const validateRegistration = require('../middleware/validateRegistration');
+const validateLogin = require('../middleware/validateLogin');
+const calculateBudgets = require('../helper/calculateBudgets');
+const validateIosRegistration = require('../middleware/ios/validateIosRegistration');
+/********************************************************
+ *                    REGISTER USER                      *
+ ********************************************************/
 router.post('/register', validateRegistration, async (req, res) => {
-  const newUser = req.body;
-  newUser.password = await hashPassword(req.body.password);
+  let user = req.body;
+  user.password = await bcrypt.hash(req.body.password, 10);
 
   try {
-    const user = await Users.addUser(newUser);
-
+    user = await Users.addUser(user);
     delete user.password;
+
+    const {
+      fat_budget_g,
+      carb_budget_g,
+      protein_budget_g,
+      caloric_budget_kcal,
+    } = calculateBudgets(user);
 
     res.status(201).json({
       message: `New account created`,
       token: generateToken(user.id),
-      user,
+      user: {
+        ...user,
+        fat_budget_g,
+        carb_budget_g,
+        protein_budget_g,
+        caloric_budget_kcal,
+      },
     });
   } catch (error) {
     res.status(500).json({
@@ -29,28 +43,63 @@ router.post('/register', validateRegistration, async (req, res) => {
   }
 });
 
+/********************************************************
+ *                      LOGIN USER                       *
+ ********************************************************/
 router.post('/login', validateLogin, async (req, res) => {
   const { email, password } = req.body;
 
   try {
     const user = await Users.getUserBy({ email });
 
-    if (!user) {
+    if (!user || !(await bcrypt.compare(password, user.password))) {
       res.status(401).json({
-        message: `No account associated with ${email}`,
-      });
-    }
-
-    if (!(await bcrypt.compare(password, user.password))) {
-      res.status(401).json({
-        message: 'Invalid password provided',
+        message: 'The email or password provided is invalid',
       });
     }
 
     delete user.password;
 
+    const {
+      fat_budget_g,
+      carb_budget_g,
+      protein_budget_g,
+      caloric_budget_kcal,
+    } = calculateBudgets(user);
+
     res.status(200).json({
-      message: `User is authenticated`,
+      message: `User authenticated`,
+      token: generateToken(user.id),
+      user: {
+        ...user,
+        fat_budget_g,
+        carb_budget_g,
+        protein_budget_g,
+        caloric_budget_kcal,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: 'Internal Server Error',
+      error: error.message,
+    });
+  }
+});
+
+/********************************************************
+ *                  REGISTER USER - IOS                 *
+ ********************************************************/
+router.post('/ios/register', validateIosRegistration, async (req, res) => {
+  const newUser = req.body;
+  newUser.password = await bcrypt.hash(req.body.password, 10);
+
+  try {
+    const user = await Users.addUser(newUser);
+
+    delete user.password;
+
+    res.status(201).json({
+      message: `New account created`,
       token: generateToken(user.id),
       user,
     });
