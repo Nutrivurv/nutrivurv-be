787c89867a5ce5a2e4eb998370e5eabbf89ea1e8
Dave Vazquez <dmvaz.li@gmail.com>
2020-07-13 22:49:03 -0400

    updates authentication middleware

--- api/middleware/authenticate.js
+++ api/middleware/authenticate.js
@@ -1,22 +1,30 @@
 const jwt = require('jsonwebtoken');
 
-const authenticate = (resolve, parent, args, ctx) => {
-  const { request } = ctx.request;
-  if (request) {
-    if (
-      request.body.query.includes('login') ||
-      request.body.query.includes('createUser')
-    ) {
-      return resolve();
-    }
-  }
-  const token = request.headers.authorization;
-  const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  if (!decoded || !decoded.userId) {
-    throw new Error(`Not authorised!`);
-  }
-  ctx.request.user_id = decoded.userId;
-  return resolve();
-};
+module.exports = (req, res, next) => {
+  const tokenHeader = req.headers.authorization;
 
-module.exports = authenticate;
+  if (tokenHeader) {
+    const [bearer, token] = tokenHeader.split(' ');
+
+    if (bearer.toUpperCase() === 'BEARER' && token) {
+      jwt.verify(
+        token,
+        process.env.JWT_SECRET || 'jwt-secret',
+        (err, decodedToken) => {
+          if (err) {
+            res.status(401).json({ message: 'invalid token' });
+          } else {
+            req.decodedJWT = decodedToken;
+            next();
+          }
+        }
+      );
+    } else {
+      res.status(400).json({
+        message: 'invalid scheme, or token missing.',
+      });
+    }
+  } else {
+    res.status(401).json({ message: 'no authorization header' });
+  }
+};
