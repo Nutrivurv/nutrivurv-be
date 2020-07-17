ec7c9e6507728cc5a3534c90fe8ffe5ab3616af2
Dave Vazquez <dmvaz.li@gmail.com>
2020-07-16 21:33:42 -0400

    changes metric units to imperial
    metric is easier for calculations.. but is cumbersome in the long run

--- db/migrations/20200616193923_create_users_table.js
+++ db/migrations/20200616193923_create_users_table.js
@@ -1,16 +1,19 @@
 exports.up = function (knex) {
   return knex.schema.createTable('users', (table) => {
     table.increments();
-    table.string('name');
-    table.string('email').unique();
-    table.string('password');
+    table.string('name').notNullable();
+    table.string('email').notNullable().unique();
+    table.string('password').notNullable();
+    2;
+    // inputs used to calculate caloric budget
     table.date('date_of_birth');
-    table.decimal('weight_kg', null);
-    table.decimal('height_cm', null);
+    table.integer('weight_lbs', null);
+    table.integer('height_ft', null);
+    table.integer('height_in', null);
     table.enum('gender', ['male', 'female']);
     table.decimal('target_weight_lbs', null);
     table.decimal('activity_level', null, 3);
-    table.integer('net_weekly_weight_change_kg');
+    table.integer('net_weekly_weight_change_lbs');
     // default USDA reccomended macronutrient ratios
     table.decimal('fat_ratio_prct', null).default(0.25);
     table.decimal('carb_ratio_prct', null).default(0.5);
