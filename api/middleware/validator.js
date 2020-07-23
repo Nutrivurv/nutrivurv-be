712af3e18c366cd136186ead7f1de5779d6db155
alesslongaretti <alesslongaretti@gmail.com>
2020-07-23 18:04:54 -0400

    added a validator middleware

--- api/middleware/validator.js
+++ api/middleware/validator.js
@@ -1,4 +1,9 @@
 module.exports = prop => (req, res, next) => {
-
+  if (req.body[prop]) {
+          next();
+        } else {
+          return res.status(400).json({ message: `${prop} is a required field.` });
+        }
+    
   };
   
