6b7986a6558aba74829b9a66a0f22c5621899948
alesslongaretti <alesslongaretti@gmail.com>
2020-07-30 00:18:50 -0400

    tests in user.spec are passing

--- tests/user.spec.js
+++ tests/user.spec.js
@@ -1,12 +1,26 @@
 const request = require('supertest');
 const server = require('../api/server.js');
 
-describe('POST / Register', () => {
-    // it('should return status 400', () => {
-    //     return request(server)
-    //       .post('/api/user/:user_id')
-    //       .then((res) => {
-    //         expect(res.status).toBe(400);
-    //       });
-    //   });
+describe('Put / update user', () => {
+  it('should return status 401', () => {
+    return request(server)
+      .put('/api/user/:user_id')
+      .then((res) => {
+        expect(res.status).toBe(401);
+      });
   });
+  it('should return status 401', () => {
+    return request(server)
+      .put('/api/user/:user_id')
+      .then((res) => {
+        expect(res.type).toBe('application/json');
+      });
+  });
+  it('should return status 401', () => {
+    return request(server)
+      .put('/api/user/:user_id')
+      .then((res) => {
+        expect(res.body.message).toEqual('no authorization header');
+      });
+  });
+});
