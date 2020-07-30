2d3e0f27eaeaf1120793fcca330b2e064b5b38d4
alesslongaretti <alesslongaretti@gmail.com>
2020-07-29 23:31:15 -0400

    finished testing auth.spec all 15 tests are passing

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -3,60 +3,62 @@ const server = require('../api/server.js');
 
 describe('POST / Register', () => {
   // Creates a new account everytime I refresh
-//   it('should return 201', async () => {
-//       return request(server).post('/api/auth/register').send({
-//           name: 'alessandra144',
-//           email: 'alessandra144@email.com',
-//           password: 'password144',
-//           date_of_birth: '4/14/1994',
-//           gender: 'female',
-//           net_weekly_weight_change_lbs: 1,
-//           weight_lbs: 130,
-//           height_ft: 5,
-//           height_in: 2,
-//           target_weight_lbs: 125,
-//           activity_level: 2,
-//           fat_ratio_prct: null,
-//           carb_ratio_prct: null,
-//           protein_ratio_prct: null
-//       })
-//       .then(res => {
-//           expect(res.status).toBe(201)
-//       })
-//   });
-    it('should return JSON type', () => {
-      return request(server)
-        .post('/api/auth/register/')
-        .then((res) => {
-          expect(res.type).toBe('application/json');
-        });
-    });
-    it('should return status 400', () => {
-      return request(server)
-        .post('/api/auth/register')
-        .then((res) => {
-          expect(res.status).toBe(400);
-        });
-    });
-      it('should return Missing credentials message', async () => {
-      return request(server).post('/api/auth/register').send({
-          email: 'alessandra3@email.com',
-          password: 'password123',
-          date_of_birth: '4/14/1994',
-          gender: 'female',
-          net_weekly_weight_change_lbs: 1,
-          weight_lbs: 130,
-          height_ft: 5,
-          height_in: 2,
-          target_weight_lbs: 125,
-          activity_level: 2,
-          fat_ratio_prct: null,
-          carb_ratio_prct: null,
-          protein_ratio_prct: null
+  //   it('should return 201', async () => {
+  //       return request(server).post('/api/auth/register').send({
+  //           name: 'alessandra144',
+  //           email: 'alessandra144@email.com',
+  //           password: 'password144',
+  //           date_of_birth: '4/14/1994',
+  //           gender: 'female',
+  //           net_weekly_weight_change_lbs: 1,
+  //           weight_lbs: 130,
+  //           height_ft: 5,
+  //           height_in: 2,
+  //           target_weight_lbs: 125,
+  //           activity_level: 2,
+  //           fat_ratio_prct: null,
+  //           carb_ratio_prct: null,
+  //           protein_ratio_prct: null
+  //       })
+  //       .then(res => {
+  //           expect(res.status).toBe(201)
+  //       })
+  //   });
+  it('should return JSON type', () => {
+    return request(server)
+      .post('/api/auth/register/')
+      .then((res) => {
+        expect(res.type).toBe('application/json');
+      });
+  });
+  it('should return status 400', () => {
+    return request(server)
+      .post('/api/auth/register')
+      .then((res) => {
+        expect(res.status).toBe(400);
+      });
+  });
+  it('should return Missing credentials message', async () => {
+    return request(server)
+      .post('/api/auth/register')
+      .send({
+        email: 'alessandra3@email.com',
+        password: 'password123',
+        date_of_birth: '4/14/1994',
+        gender: 'female',
+        net_weekly_weight_change_lbs: 1,
+        weight_lbs: 130,
+        height_ft: 5,
+        height_in: 2,
+        target_weight_lbs: 125,
+        activity_level: 2,
+        fat_ratio_prct: null,
+        carb_ratio_prct: null,
+        protein_ratio_prct: null,
       })
-      .then(res => {
-          expect(res.body).toEqual({message: 'Missing credentials'})
-      })
+      .then((res) => {
+        expect(res.body).toEqual({ message: 'Missing credentials' });
+      });
   });
 });
 
@@ -111,91 +113,96 @@ describe('POST / Login', () => {
 });
 
 describe('POST / Register ios', () => {
-    // it('should return 201', async () => {
-    //     return request(server).post('/api/auth/ios/register').send({
-    //       name: 'alessandra144',
-    //       email: 'alessandra144@email.com',
-    //       password: 'password123',
-    //       date_of_birth: '4/14/1994',
-    //       gender: 'female',
-    //       net_weekly_weight_change_lbs: 1,
-    //       weight_lbs: 130,
-    //       height_ft: 5,
-    //       height_in: 2,
-    //       target_weight_lbs: 125,
-    //       activity_level: 2,
-    //       fat_ratio_prct: null,
-    //       carb_ratio_prct: null,
-    //       protein_ratio_prct: null
-    //     })
-    //     .then( res => {
-    //         expect(res.status).toBe(201)
-    //     })
-    // })
-        it('should return status 409', async () => {
-          return request(server)
-            .post('/api/auth/ios/register')
-            .send({
-              name: 'alessandra4',
-              email: 'alessandra4@email.com',
-              password: 'password123',
-              date_of_birth: '4/14/1994',
-              gender: 'female',
-              net_weekly_weight_change_lbs: 1,
-              weight_lbs: 130,
-              height_ft: 5,
-              height_in: 2,
-              target_weight_lbs: 125,
-              activity_level: 2,
-              fat_ratio_prct: null,
-              carb_ratio_prct: null,
-              protein_ratio_prct: null,
-            })
-            .then((res) => {
-              expect(res.status).toBe(409);
-            });
+  // it('should return 201', async () => {
+  //     return request(server).post('/api/auth/ios/register').send({
+  //       name: 'alessandra144',
+  //       email: 'alessandra144@email.com',
+  //       password: 'password123',
+  //       date_of_birth: '4/14/1994',
+  //       gender: 'female',
+  //       net_weekly_weight_change_lbs: 1,
+  //       weight_lbs: 130,
+  //       height_ft: 5,
+  //       height_in: 2,
+  //       target_weight_lbs: 125,
+  //       activity_level: 2,
+  //       fat_ratio_prct: null,
+  //       carb_ratio_prct: null,
+  //       protein_ratio_prct: null
+  //     })
+  //     .then( res => {
+  //         expect(res.status).toBe(201)
+  //     })
+  // })
+  it('should return status 409', async () => {
+    return request(server)
+      .post('/api/auth/ios/register')
+      .send({
+        name: 'alessandra4',
+        email: 'alessandra4@email.com',
+        password: 'password123',
+        date_of_birth: '4/14/1994',
+        gender: 'female',
+        net_weekly_weight_change_lbs: 1,
+        weight_lbs: 130,
+        height_ft: 5,
+        height_in: 2,
+        target_weight_lbs: 125,
+        activity_level: 2,
+        fat_ratio_prct: null,
+        carb_ratio_prct: null,
+        protein_ratio_prct: null,
+      })
+      .then((res) => {
+        expect(res.status).toBe(409);
+      });
+  });
+  it('should return message Account with email already exist', async () => {
+    return request(server)
+      .post('/api/auth/ios/register')
+      .send({
+        name: 'alessandra4',
+        email: 'alessandra4@email.com',
+        password: 'password123',
+        date_of_birth: '4/14/1994',
+        gender: 'female',
+        net_weekly_weight_change_lbs: 1,
+        weight_lbs: 130,
+        height_ft: 5,
+        height_in: 2,
+        target_weight_lbs: 125,
+        activity_level: 2,
+        fat_ratio_prct: null,
+        carb_ratio_prct: null,
+        protein_ratio_prct: null,
+      })
+      .then((res) => {
+        expect(res.body).toEqual({
+          message: 'Account with email alessandra4@email.com is already exists',
         });
-        it('should return message Account with email already exist', async () => {
-          return request(server)
-            .post('/api/auth/ios/register')
-            .send({
-              name: 'alessandra4',
-              email: 'alessandra4@email.com',
-              password: 'password123',
-              date_of_birth: '4/14/1994',
-              gender: 'female',
-              net_weekly_weight_change_lbs: 1,
-              weight_lbs: 130,
-              height_ft: 5,
-              height_in: 2,
-              target_weight_lbs: 125,
-              activity_level: 2,
-              fat_ratio_prct: null,
-              carb_ratio_prct: null,
-              protein_ratio_prct: null,
-            })
-            .then((res) => {
-              expect(res.body).toEqual({
-                message:
-                  'Account with email alessandra4@email.com is already exists',
-              });
-            });
+      });
+  });
+  it('should return message Missing Credentials', async () => {
+    return request(server)
+      .post('/api/auth/ios/register')
+      .then((res) => {
+        expect(res.body).toEqual({
+          message: 'Missing credentials',
         });
-        it('should return message Missing Credentials', async () => {
-            return request(server)
-              .post('/api/auth/ios/register')
-              .then((res) => {
-                expect(res.body).toEqual({
-                  message:
-                    'Missing credentials',
-                });
-              });
-          });
-          it('should return status 400', async () => {
-            return request(server)
-              .post('/api/auth/ios/register')
-              .then((res) => {
-                expect(res.status).toBe(400);
-              });
-          });
+      });
+  });
+  it('should return status 400', async () => {
+    return request(server)
+      .post('/api/auth/ios/register')
+      .then((res) => {
+        expect(res.status).toBe(400);
+      });
+  });
+  it('should return JSON type', async () => {
+    return request(server)
+      .post('/api/auth/ios/register')
+      .then((res) => {
+        expect(res.type).toBe('application/json');
+      });
+  });
 });
