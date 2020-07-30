f55439f389871acc12b318706a984e72da144e7b
alesslongaretti <alesslongaretti@gmail.com>
2020-07-29 22:54:45 -0400

    added test for login 200 status

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const server = require('../api/server.js');
+const generateToken = require('../api/middleware/generateToken');
 
 describe('POST / Register', () => {
   // Creates a new account everytime I refresh
@@ -24,27 +25,52 @@ describe('POST / Register', () => {
   //         expect(res.status).toBe(201)
   //     })
   // });
-  it('should return message - problem with db', () => {
-    return request(server)
-      .post('/api/auth/register')
-      .catch((res) => {
-        expect(res.body).toEqual({ message: 'problem with the db' });
-      });
-  });
-  it('should return 500', () => {
-    return request(server)
-      .post('/api/auth/register')
-      .catch((res) => {
-        expect(res.status).toBe(500);
-      });
+    it('should return JSON type', () => {
+      return request(server)
+        .post('/api/auth/register/')
+        .then((res) => {
+          expect(res.type).toBe('application/json');
+        });
+    });
+    it('should return 400', () => {
+      return request(server)
+        .post('/api/auth/register')
+        .then((res) => {
+          expect(res.status).toBe(400);
+        });
+    });
+      it('should return Missing credentials message', async () => {
+      return request(server).post('/api/auth/register').send({
+          email: 'alessandra3@email.com',
+          password: 'password123',
+          date_of_birth: '4/14/1994',
+          gender: 'female',
+          net_weekly_weight_change_lbs: 1,
+          weight_lbs: 130,
+          height_ft: 5,
+          height_in: 2,
+          target_weight_lbs: 125,
+          activity_level: 2,
+          fat_ratio_prct: null,
+          carb_ratio_prct: null,
+          protein_ratio_prct: null
+      })
+      .then(res => {
+          expect(res.body).toEqual({message: 'Missing credentials'})
+      })
   });
 });
 
 describe('POST / Login', () => {
-    it('should return message Missing credentials', () => {
-        return request(server).post('/api/auth/login')
-            .catch(res => {
-                expect(res.body).toEqual({message: 'Missing credentials'})
-            });
-    })
-})
+  it('should return 200', async () => {
+    return request(server)
+      .post('/api/auth/login')
+      .send({
+        email: 'alessandra2@email.com',
+        password: 'password123',
+      })
+      .then((res) => {
+        expect(res.status).toBe(200);
+      });
+  });
+});
