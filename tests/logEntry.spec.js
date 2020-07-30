6e52456170b0749202eb1d733a672bbb2c9438bd
alesslongaretti <alesslongaretti@gmail.com>
2020-07-30 00:16:08 -0400

    finished tests for logEntry.spec

--- tests/logEntry.spec.js
+++ tests/logEntry.spec.js
@@ -72,27 +72,77 @@ describe('GET / getById', () => {
 });
 
 describe('POST / add log entry', () => {
-    it('should be JSON type', () => {
-      return request(server)
-        .post('/api/log/')
-        .then((res) => {
-          expect(res.type).toBe('application/json');
-        });
-    });
-    it('should be status 401', () => {
-      return request(server)
-        .post('/api/log/')
-        .then((res) => {
-          expect(res.status).toBe(401);
-        });
-    });
-    it('should return no authorization header message', () => {
-      return request(server)
-        .post('/api/log/')
-        .then((res) => {
-          expect(res.body.message).toEqual('no authorization header');
-        });
-    });
+  it('should be JSON type', () => {
+    return request(server)
+      .post('/api/log/')
+      .then((res) => {
+        expect(res.type).toBe('application/json');
+      });
   });
+  it('should be status 401', () => {
+    return request(server)
+      .post('/api/log/')
+      .then((res) => {
+        expect(res.status).toBe(401);
+      });
+  });
+  it('should return no authorization header message', () => {
+    return request(server)
+      .post('/api/log/')
+      .then((res) => {
+        expect(res.body.message).toEqual('no authorization header');
+      });
+  });
+});
+
+describe('Delete / delete log entry', () => {
+  it('should be JSON type', () => {
+    return request(server)
+      .delete('/api/log/:log_entry_id')
+      .then((res) => {
+        expect(res.type).toBe('application/json');
+      });
+  });
+  it('should be status 401', () => {
+    return request(server)
+      .delete('/api/log/:log_entry_id')
+      .then((res) => {
+        expect(res.status).toBe(401);
+      });
+  });
+  it('should return no authorization header message', () => {
+    return request(server)
+      .delete('/api/log/:log_entry_id')
+      .then((res) => {
+        expect(res.body.message).toEqual('no authorization header');
+      });
+  });
+});
+
+describe('Put / update log entry', () => {
+  it('should be JSON type', () => {
+    return request(server)
+      .put('/api/log/:log_entry_id')
+      .then((res) => {
+        expect(res.type).toBe('application/json');
+      });
+  });
+  it('should be status 401', () => {
+    return request(server)
+      .put('/api/log/:log_entry_id')
+      .then((res) => {
+        expect(res.status).toBe(401);
+      });
+  });
+  it('should return no authorization header message', () => {
+    return request(server)
+      .put('/api/log/:log_entry_id')
+      .then((res) => {
+        expect(res.body.message).toEqual('no authorization header');
+      });
+  });
+});
+
+  
 
   
---
+++ tests/user.spec.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+const server = require('../api/server.js');
+
+describe('POST / Register', () => {
+    // it('should return status 400', () => {
+    //     return request(server)
+    //       .post('/api/user/:user_id')
+    //       .then((res) => {
+    //         expect(res.status).toBe(400);
+    //       });
+    //   });
+  });
